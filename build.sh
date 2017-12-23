787a11bcd6cee2ad131e7aa12936ff5e91fadf6e
SecurityAddicted <alex@securityaddicted.com>
2017-12-23 15:13:56 +0000

    Build scripts added

---
+++ build.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# simple bash script for executing build
+
+# root directory of NetHunter shieldtablet git repo (default is this script's location)
+RDIR=$(pwd)
+
+[ "$VER" ] ||
+# version number
+VER=$(cat "$RDIR/VERSION")
+
+# directory containing cross-compile arm toolchain
+TOOLCHAIN=$HOME/build/toolchain/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf
+
+CPU_THREADS=$(grep -c "processor" /proc/cpuinfo)
+# amount of cpu threads to use in kernel make process
+THREADS=$((CPU_THREADS + 1))
+
+############## SCARY NO-TOUCHY STUFF ###############
+
+ABORT()
+{
+	[ "$1" ] && echo "Error: $*"
+	exit 1
+}
+
+export ARCH=arm
+export CROSS_COMPILE=$TOOLCHAIN/bin/arm-linux-gnueabihf-
+
+[ -x "${CROSS_COMPILE}gcc" ] ||
+ABORT "Unable to find gcc cross-compiler at location: ${CROSS_COMPILE}gcc"
+
+[ "$TARGET" ] || TARGET=nethunter
+[ "$1" ] && DEVICE=$1
+[ "$DEVICE" ] || DEVICE=shieldtablet
+
+DEFCONFIG=${TARGET}_${DEVICE}_defconfig
+
+[ -f "$RDIR/arch/$ARCH/configs/${DEFCONFIG}" ] ||
+ABORT "Config $DEFCONFIG not found in $ARCH configs!"
+
+export LOCALVERSION=$TARGET-$DEVICE-$VER
+
+CLEAN_BUILD()
+{
+	echo "Cleaning build..."
+	rm -rf build
+}
+
+SETUP_BUILD()
+{
+	echo "Creating kernel config for $LOCALVERSION..."
+	mkdir -p build
+	make -C "$RDIR" O=build "$DEFCONFIG" \
+		|| ABORT "Failed to set up build"
+}
+
+BUILD_KERNEL()
+{
+	echo "Starting build for $LOCALVERSION..."
+	while ! make -C "$RDIR" O=build -j"$THREADS"; do
+		read -rp "Build failed. Retry? " do_retry
+		case $do_retry in
+			Y|y) continue ;;
+			*) return 1 ;;
+		esac
+	done
+}
+
+INSTALL_MODULES() {
+	echo "Installing kernel modules to build/lib/modules..."
+	make -C "$RDIR" O=build \
+		INSTALL_MOD_PATH="." \
+		INSTALL_MOD_STRIP=1 \
+		modules_install
+	rm build/lib/modules/*/build build/lib/modules/*/source
+}
+
+cd "$RDIR" || ABORT "Failed to enter $RDIR!"
+
+CLEAN_BUILD &&
+SETUP_BUILD &&
+BUILD_KERNEL &&
+INSTALL_MODULES &&
+echo "Finished building $LOCALVERSION!"
